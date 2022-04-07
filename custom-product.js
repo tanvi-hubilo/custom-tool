c0c946083296017761e1a6c7e6e41da0087af5b9
tanvi-hubilo <tanvi@hubilo.com>
2022-04-07 13:36:11 +0530

    Custom-js file

--- custom-product.js
+++ custom-product.js
@@ -1,4 +1,5 @@
-const editorTemplate = '<button id="addProduct" class="button">Add Logo</button>';
+const editorTemplate =
+  '<button id="addProduct" class="button">Add Logo</button>';
 
 const productItemsTemplate = _.template(`
 <% _.forEach(products, function(item) { %>
@@ -19,7 +20,10 @@ const modalTemplate = function (data) {
         </div>
         <div class="modal-body">
         <div>
-        <p>Images List will be here</p>
+        <div class="search-box">
+                     <input type="text" class="form-control" placeholder="Search by speaker name" id="search-bar" style="width: 78%" />
+                    <button id="search-btn" class="button" style="width: 20%">Search</button>
+        </div>   
         </div>
           <div class="products-list">
             ${productItemsTemplate(data)}
@@ -39,16 +43,20 @@ const toolTemplate = function (values, isViewer = false) {
       values.speakerImage.url
     }" style="width: 100%; object-fit: contain; border-top-left-radius: 4px; border-top-right-radius: 4px;" />
   </div>
-  ${isViewer ? modalTemplate({ products: values.data.products }) : ''}`;
+  ${isViewer ? modalTemplate({ products: values.data.products }) : ""}`;
 };
 
 const toolEmailTemplate = function (values, isViewer = false) {
   //   console.log('values IMPO', values);
   return `
     <table id="${
-      values?.speakerLibrary?.selected?.id ? values?.speakerLibrary?.selected?.id : ''
+      values?.speakerLibrary?.selected?.id
+        ? values?.speakerLibrary?.selected?.id
+        : ""
     }" speakerId="${
-    values?.speakerLibrary?.selected?.id ? values?.speakerLibrary?.selected?.id : ''
+    values?.speakerLibrary?.selected?.id
+      ? values?.speakerLibrary?.selected?.id
+      : ""
   }" cellspacing="0" cellpadding="0" style="position:relative;min-width:0;word-wrap:break-word;background-color:#fff;background-clip:border-box;border:1px solid rgba(0,0,0,.125);border-radius:4px;margin:auto;text-align:center;">
       <tbody>
       </tbody>
@@ -57,32 +65,32 @@ const toolEmailTemplate = function (values, isViewer = false) {
 };
 
 const showModal = function () {
-  const modal = document.getElementById('product_library_modal');
-  modal.classList.add('show');
+  const modal = document.getElementById("product_library_modal");
+  modal.classList.add("show");
 };
 
 const hideModal = function () {
-  const modal = document.getElementById('product_library_modal');
-  modal.classList.remove('show');
+  const modal = document.getElementById("product_library_modal");
+  modal.classList.remove("show");
 };
 
 unlayer.registerPropertyEditor({
-  name: 'speaker_library',
-  layout: 'bottom',
+  name: "speaker_library",
+  layout: "bottom",
   Widget: unlayer.createWidget({
     render(value, updateValue, data) {
       return editorTemplate;
     },
     mount(node, value, updateValue, data) {
-      const addButton = node.querySelector('#addProduct');
+      const addButton = node.querySelector("#addProduct");
       addButton.onclick = function () {
         showModal();
         setTimeout(() => {
           // We are using event bubling to capture clicked item instead of registering click event on all product items.
-          const selectButton = document.querySelector('.products-list');
+          const selectButton = document.querySelector(".products-list");
           if (!selectButton) return;
           selectButton.onclick = function (e) {
-            if (e.target.id === 'product-item') {
+            if (e.target.id === "product-item") {
               // If user clicks on product item
               // Find selected item from products list
               const selectedProduct = data.products.find(
@@ -92,7 +100,7 @@ unlayer.registerPropertyEditor({
             } else {
               // If user click on child of product item (e.g. title, price, image or desctiption)
               const parent = e.target.parentElement;
-              if (parent && parent.id !== 'product-item') return;
+              if (parent && parent.id !== "product-item") return;
               const selectedProduct = data.products.find(
                 (item) => item.id === parseInt(parent.dataset.uuid)
               );
@@ -100,25 +108,33 @@ unlayer.registerPropertyEditor({
             }
             hideModal();
             // This is a hack to close property editor right bar on selecting an item from products list.
-            const outerBody = document.querySelector('#u_body');
+            const outerBody = document.querySelector("#u_body");
             outerBody.click();
           };
           /* Register event listeners for search */
-          const searchBar = document.querySelector('#search-bar');
-          const searchButton = document.querySelector('#search-btn');
-          const closeBtn = document.querySelector('#modalCloseBtn');
+          const searchBar = document.querySelector("#search-bar");
+          const searchButton = document.querySelector("#search-btn");
+          const closeBtn = document.querySelector("#modalCloseBtn");
           searchButton.onclick = function (e) {
-            const list = document.querySelector('#product_library_modal .products-list');
+            const list = document.querySelector(
+              "#product_library_modal .products-list"
+            );
             let filteredItem;
             let productsListHtml;
             if (list && data && data.products) {
-              if (searchBar.value === '') {
-                productsListHtml = productItemsTemplate({ products: data.products });
+              if (searchBar.value === "") {
+                productsListHtml = productItemsTemplate({
+                  products: data.products,
+                });
               } else {
                 filteredItem = data.products.filter((item) =>
-                  item.name.toLowerCase().includes(searchBar.value.toLowerCase())
+                  item.name
+                    .toLowerCase()
+                    .includes(searchBar.value.toLowerCase())
                 );
-                productsListHtml = productItemsTemplate({ products: filteredItem });
+                productsListHtml = productItemsTemplate({
+                  products: filteredItem,
+                });
               }
               list.innerHTML = productsListHtml;
             }
@@ -131,26 +147,26 @@ unlayer.registerPropertyEditor({
 });
 
 unlayer.registerTool({
-  name: 'speaker_tool',
-  label: 'Speaker',
-  icon: 'fa-user-circle',
-  supportedDisplayModes: ['web', 'email'],
+  name: "speaker_tool",
+  label: "Speaker",
+  icon: "fa-user-circle",
+  supportedDisplayModes: ["web", "email"],
   options: {
     speakerContent: {
-      title: 'Speaker Content',
+      title: "Speaker Content",
       position: 1,
       options: {
         speakerLibrary: {
-          label: 'Add Speaker from store',
-          defaultValue: '',
-          widget: 'speaker_library',
+          label: "Add Speaker from store",
+          defaultValue: "",
+          widget: "speaker_library",
         },
         speakerImage: {
-          label: 'Speaker Image',
+          label: "Speaker Image",
           defaultValue: {
-            url: 'https://s3.amazonaws.com/unroll-images-production/projects%2F167%2F1643875820464-188690',
+            url: "https://s3.amazonaws.com/unroll-images-production/projects%2F167%2F1643875820464-188690",
           },
-          widget: 'image',
+          widget: "image",
         },
       },
     },
@@ -160,7 +176,7 @@ unlayer.registerTool({
     // Transform the values here
     // We will update selected values in property editor here
     const newValues =
-      name === 'speakerLibrary'
+      name === "speakerLibrary"
         ? {
             ...values,
             speakerTitle: value.selected.name,
