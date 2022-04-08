e1cfa42f86d2735e432d0ae57012dfaae5e739df
tanvi-hubilo <tanvi@hubilo.com>
2022-04-08 19:13:41 +0530

    Changes given by sagar

--- custom-logo-tool.js
+++ custom-logo-tool.js
@@ -1,6 +1,4 @@
-const editorTemplate =
-  '<button id="addLogo" class="button">Add Logo</button>';
-
+const editorTemplate = `<button id="addLogo" class="button" style="color: ${theme.secondary};background-color:${theme.primary};">Add Logo</button>`;
 const logoItemsTemplate = _.template(`
 <% _.forEach(logos, function(item) { %>
   <div class="product-item" id="logo-item" data-uuid='<%= item.id %>' data-image="<%= item.img %>" >
@@ -40,54 +38,55 @@ const modalTemplate = function (data) {
 const toolTemplate = function (values, isViewer = false) {
   return `<div class="product-card" style="position:relative;display:table;min-width:0;word-wrap:break-word;background-color:#fff;background-clip:border-box;border:1px solid rgba(0,0,0,.125);border-radius:4px;margin:auto;text-align:center;">
     <img src="${
-      values.logo_image.url
+      values?.logo_image?.url ? values?.logo_image?.url : 'https://s3.amazonaws.com/unroll-images-production/projects%2F167%2F1643875820464-188690'
     }" style="width: 100%; object-fit: contain; border-top-left-radius: 4px; border-top-right-radius: 4px;" />
   </div>
-  ${isViewer ? modalTemplate({ logos: values.data.logos }) : ""}`;
+  ${isViewer ? modalTemplate({ logos: values.data.logos }) : ''}`;
 };
 
 const toolEmailTemplate = function (values, isViewer = false) {
-  //   console.log('values IMPO', values);
   return `
-    <table id="${
-      values?.id
-        ? values?.id
-        : ""
-    }"
-  }" cellspacing="0" cellpadding="0" style="position:relative;min-width:0;word-wrap:break-word;background-color:#fff;background-clip:border-box;border:1px solid rgba(0,0,0,.125);border-radius:4px;margin:auto;text-align:center;">
+    <table logoId="${
+      values?.logoLibrary?.selected?.id ? values?.logoLibrary?.selected?.id : ''
+    }" cellspacing="0" cellpadding="0" style="position:relative;min-width:0;word-wrap:break-word;background-color:#fff;background-clip:border-box;border:1px solid rgba(0,0,0,.125);border-radius:4px;margin:auto;text-align:center;">
       <tbody>
+        <tr><td width="100%"><img id="${values?.logoLibrary?.selected?.id}-logo" src="${
+     values?.logo_image?.url
+       ? values?.logo_image?.url
+       : 'https://s3.amazonaws.com/unroll-images-production/projects%2F167%2F1643875820464-188690'
+   }" style="width: 100%; object-fit: contain; border-top-left-radius: 4px; border-top-right-radius: 4px;" /></td></tr>
       </tbody>
     </table>
-  `;
+  `;  
 };
 
 const showModal = function () {
-  const modal = document.getElementById("logo_library_modal");
-  modal.classList.add("show");
+  const modal = document.getElementById('logo_library_modal');
+  modal.classList.add('show');
 };
 
 const hideModal = function () {
-  const modal = document.getElementById("logo_library_modal");
-  modal.classList.remove("show");
+  const modal = document.getElementById('logo_library_modal');
+  modal.classList.remove('show');
 };
 
 unlayer.registerPropertyEditor({
-  name: "logo_library",
-  layout: "bottom",
+  name: 'logo_library',
+  layout: 'bottom',
   Widget: unlayer.createWidget({
     render(value, updateValue, data) {
       return editorTemplate;
     },
     mount(node, value, updateValue, data) {
-      const addButton = node.querySelector("#addLogo");
+      const addButton = node.querySelector('#addLogo');
       addButton.onclick = function () {
         showModal();
         setTimeout(() => {
           // We are using event bubling to capture clicked item instead of registering click event on all product items.
-          const selectButton = document.querySelector(".products-list");
+          const selectButton = document.querySelector('.products-list');
           if (!selectButton) return;
           selectButton.onclick = function (e) {
-            if (e.target.id === "logo-item") {
+            if (e.target.id === 'logo-item') {
               // If user clicks on logo item
               // Find selected item from logo list
               const selectedProduct = data.logos.find(
@@ -97,7 +96,7 @@ unlayer.registerPropertyEditor({
             } else {
               // If user click on child of product item (e.g. title, price, image or desctiption)
               const parent = e.target.parentElement;
-              if (parent && parent.id !== "logo-item") return;
+              if (parent && parent.id !== 'logo-item') return;
               const selectedProduct = data.logos.find(
                 (item) => item.id === parseInt(parent.dataset.uuid)
               );
@@ -105,11 +104,11 @@ unlayer.registerPropertyEditor({
             }
             hideModal();
             // This is a hack to close property editor right bar on selecting an item from logos list.
-            const outerBody = document.querySelector("#u_body");
+            const outerBody = document.querySelector('#u_body');
             outerBody.click();
           };
           /* Register event listeners for search */
-          const closeBtn = document.querySelector("#modalCloseBtn");
+          const closeBtn = document.querySelector('#modalCloseBtn');
           closeBtn.onclick = hideModal;
         }, 200);
       };
@@ -118,26 +117,19 @@ unlayer.registerPropertyEditor({
 });
 
 unlayer.registerTool({
-  name: "logo_tool",
-  label: "Logo",
-  icon: "fa-user-circle",
-  supportedDisplayModes: ["web", "email"],
+  name: 'logo_tool',
+  label: 'Logos',
+  icon: 'fa-calendar',
+  supportedDisplayModes: ['web', 'email'],
   options: {
     logoContent: {
-      title: "Logo Content",
+      title: 'Logo Content',
       position: 1,
       options: {
         logoLibrary: {
           label: 'Add Logo from store',
           defaultValue: '',
           widget: 'logo_library',
-        },
-        logo_image: {
-          label: "Logo Image",
-          defaultValue: {
-            url: "https://s3.amazonaws.com/unroll-images-production/projects%2F167%2F1643875820464-188690",
-          },
-          widget: "image",
         },
       },
     },
@@ -147,12 +139,12 @@ unlayer.registerTool({
     // Transform the values here
     // We will update selected values in property editor here
     const newValues =
-      name === "logoLibrary"
+      name === 'logoLibrary'
         ? {
             ...values,
-            id : value.selected.id,
+            id: value?.selected?.id,
             logo_image: {
-              url: value.selected.img,
+              url: value?.selected?.img,
             },
           }
         : {
